789f2ddf4d05a011bb02de86f2793015299b3897
Hossamsay <hossam.sayed.abdallah@gmail.com>
2020-03-08 22:58:38 +0200

    my ASP.net app

---
+++ ContosoUniversity/DAL/SchoolContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using ContosoUniversity.Models;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace ContosoUniversity.DAL
+{
+    public class SchoolContext : DbContext
+    {
+        //make constructor to start database first and make inhertance from Dbcontext constructors by using base word
+        public SchoolContext() : base("SchoolContext")
+        { }
+        //make prop for every base class but make type Dbset to create tables and make table name (studets or Enrollments like this) collection name
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<OfficeAssignment> OfficeAssignments { get; set; }
+
+        //we make this to remove the way of naming tables in database to make it single instead of collection to make it like base classes 
+        //this method come from Dbcontext and make it override 
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            //create pure join table in runtime not in database which has foregin keys only and will be compiste primary key 
+            modelBuilder.Entity<Course>().HasMany(c => c.Instructors).WithMany(i => i.Courses).Map(t => t.MapLeftKey("CourseID")
+            .MapRightKey("InstructorID").ToTable("CourseInstructor"));
+        }
+    }
+}
---
+++ ContosoUniversity/Models/Student.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        //pk
+        public int ID { get; set; }
+        [Required(ErrorMessage ="Last Name is Required")]
+        [StringLength(30,ErrorMessage ="Last Name can't be more than 30 chars")]
+        [Display(Name ="Last Name")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$",ErrorMessage ="Last Name must start with Upper Chars")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "First Name is Required")]
+        [StringLength(30, ErrorMessage = "First Name can't be more than 30 chars")]
+        [Display(Name = "First Name")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$", ErrorMessage = "First Name must start with Upper Chars")]
+        [Column("FirstName")]
+        public string FirstMidName { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
+        public DateTime EnrollmentDate { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstMidName;
+            }
+        }
+        //navigation property
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
